935a5751de97e87629cf4f076ea8d9610896c298
Sayan Chakraborty <mail.sayanc@gmail.com>
2020-02-12 18:05:22 +0530

    Revert the ets logging

--- lib/exmqttc.ex
+++ lib/exmqttc.ex
@@ -154,10 +154,7 @@ defmodule Exmqttc do
 
   @impl GenServer
   def handle_cast({:publish_message, topic, payload, opts}, state = {mqtt_pid, _callback_pid}) do
-    res = :emqttc.publish(mqtt_pid, topic, payload, opts)
-    if (String.contains?(topic, "5a6740542b6a07160ec6704d")) do
-      :ets.insert(:publish_time, {topic <> "exmqttc", "#{DateTime.utc_now()}, res: #{inspect(res)}"})
-    end
+    :emqttc.publish(mqtt_pid, topic, payload, opts)
     {:noreply, state}
   end
 
